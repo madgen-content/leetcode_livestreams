a4c156ab05a3beb9ad47f69d01998695f0687f50
Michael <madgencontent@gmail.com>
2019-12-16 19:32:52 -0500

    monotonicity notes

--- 12_16_monotonic_list.py
+++ 12_16_monotonic_list.py
@@ -1,4 +1,18 @@
+# could definitely optimize this more
+# but this is a dumb problem and not gonna bother
+# this'll do
 class Solution:
     def isMonotonic(self, A: List[int]) -> bool:
         diffs = [A[i] - A[i-1] for i in range(1, len(A))]
         return all(map(lambda x: x <= 0, diffs)) or all(map(lambda x: x >= 0, diffs))
+
+# awesome oneliner from discussion
+# realizing monotonicity implies either sorted form is amazing
+# i don't think this relies on the fact that python's sort is stable
+# but if you solved a similar problem on objects or dicts by a certain member
+# this would still work because of that
+# you'd have to have a key=lambda clause tho
+
+class Solution:
+    def isMonotonic(self, A: List[int]) -> bool:
+        return True if A == sorted(A) or A == sorted(A, reverse = True) else False
