cc477d5d8ffa2f524a2d823dc82fdfae683a9790
Michael <madgencontent@gmail.com>
2019-12-30 23:31:03 -0500

    working solution for list intersection

---
+++ 12_30_intersection_2_linked_lists.py
@@ -0,0 +1,52 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def fix_list(p):
+    while p != None:
+        if isinstance(p.val, tuple):
+            p.val = p.val[0]
+        p = p.next
+    return
+
+def fix_both(a, b):
+    fix_list(a)
+    fix_list(b)
+    return
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        
+        p1 = headA
+        p2 = headB
+
+        if p1 == p2:
+            return p1
+
+        while True:
+            if p1 != None:
+                if isinstance(p1.val, tuple):
+                    fix_both(headA, headB)
+                    return p1
+                else:
+                    p1.val = (p1.val,)
+                    p1 = p1.next
+            
+            if p2 != None:
+                if isinstance(p2.val, tuple):
+                    fix_both(headA, headB)
+                    return p2
+                else:
+                    p2.val = (p2.val,)
+                    p2 = p2.next
+            
+            if p1 == None and p2 == None:
+                fix_both(headA, headB)
+                return None
+
+            
+            
+
+
