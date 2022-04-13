75f1d1304221268119a7c7705b3221990514c7bd
Michael <madgencontent@gmail.com>
2022-04-13 11:25:46 -0700

    daily: 1427

---
+++ season_2/04_13_string_shift_1427.py
@@ -0,0 +1,13 @@
+class Solution:
+    def stringShift(self, s: str, shift: List[List[int]]) -> str:
+        if s == "":
+            return ""
+        
+        shift_total = sum([(1 if dir==0 else -1)*size for dir, size in shift])
+
+        if shift_total == 0:
+            return s
+        else:
+            print(shift_total)
+            shift_total %= len(s)
+            return s[shift_total:] + s[:shift_total]
