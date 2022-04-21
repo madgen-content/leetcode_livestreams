d1803b71e828cde82d121a73e5db09863039ee6d
Michael <madgencontent@gmail.com>
2022-04-21 09:51:39 -0700

    daily: 21

---
+++ season_2/04_20_word_break_139.py
@@ -0,0 +1,24 @@
+# copy pasted the brute force because its worth knowing
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        def wordBreakRecur(s: str, word_dict: Set[str], start: int):
+            if start == len(s):
+                return True
+            for end in range(start + 1, len(s) + 1):
+                if s[start:end] in word_dict and wordBreakRecur(s, word_dict, end):
+                    return True
+            return False
+
+        return wordBreakRecur(s, set(wordDict), 0)
+
+# my attempt?
+from typing import *
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # degenerate case check
+        s1 = set(s)
+        s2 = set(''.join(wordDict))
+        if len(s1.symmetric_difference(s2)) > 0:
+            return False
+        
+        
---
+++ season_2/04_21_merge_lists_21.py
@@ -0,0 +1,45 @@
+# Definition for singly-linked list.
+from typing import *
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list2:
+            return list1
+        if not list1:
+            return list2
+            
+        if list1.val < list2.val:
+            cur = list1
+            list1 = list1.next
+        else:
+            cur = list2
+            list2 = list2.next
+        
+        root = cur
+        while list1 or list2:
+
+            if list2 is None:
+                cur.next = list1
+                cur = cur.next
+                list1 = list1.next
+                continue
+            
+            if list1 is None:
+                cur.next = list2
+                cur = cur.next
+                list2 = list2.next
+                continue
+
+            if list1.val < list2.val:
+                cur.next = list1
+                cur = cur.next
+                list1 = list1.next
+            else:
+                cur.next = list2
+                cur = cur.next
+                list2 = list2.next
+
+        return root
